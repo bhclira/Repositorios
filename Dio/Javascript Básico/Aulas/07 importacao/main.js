09c2776d17090dd8cee6ee6aa0cf5293f7199c85
bhclira <bhcl@cesar.school>
2024-12-10 11:21:55 -0300

    atualiza lógica para encontrar o maior valor sorteado e melhora a legibilidade do código

--- Dio/Javascript Básico/Aulas/07 importacao/main.js
+++ Dio/Javascript Básico/Aulas/07 importacao/main.js
@@ -11,16 +11,27 @@ Dados de entrada:
 Saida:
 98
 */
-const {gets, print} = require('./funcoes_auxiliares.js');
 
+// Importa as funções gets e print do arquivo funcoes_auxiliares.js
+const { gets, print } = require('./funcoes_auxiliares.js');
+
+// Recebe a quantidade de alunos (números sorteados) através da função gets
 const quantidadeDeAlunos = gets();
+
+// Inicializa a variável maiorValorEncontrado com 0
 let maiorValorEncontrado = 0;
 
+// Loop para iterar sobre a quantidade de alunos
 for (let i = 0; i < quantidadeDeAlunos; i++) {
+    // Recebe o número sorteado para o aluno atual
     const numeroSorteado = gets();
+    
+    // Verifica se o número sorteado é maior que o maior valor encontrado até agora
     if (numeroSorteado > maiorValorEncontrado) {
+        // Atualiza o maior valor encontrado
         maiorValorEncontrado = numeroSorteado;
     }
-};
+}
 
+// Imprime o maior valor encontrado
 print(maiorValorEncontrado);
